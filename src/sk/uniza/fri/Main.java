3d9517effe6d8fe0ec74b04fd9f2ab1de4b970fd
UhlarM <matusmonogram@gmail.com>
2024-11-15 14:05:54 +0100

    first commit

---
+++ src/sk/uniza/fri/Main.java
@@ -0,0 +1,14 @@
+package sk.uniza.fri;
+/**
+ * Created by IntelliJ IDEA.
+ * User: matus
+ * Date: 15. 11. 2024
+ * Time: 13:25
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+    }
+}
